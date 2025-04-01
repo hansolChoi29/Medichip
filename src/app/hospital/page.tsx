49a7276c67801e6ccf0e4ea771931e993e8f6222
hansolChoi29 <ggksthf29@gmail.com>
2025-04-01 13:29:15 +0900

    chore: 타이틀만 넣어줌

--- src/app/hospital/page.tsx
+++ src/app/hospital/page.tsx
@@ -1,3 +1,12 @@
-export function HospitalPage() {
-  return <div>hospitalpageeeee</div>;
+export default function HospitalPage() {
+  return (
+    <section>
+      <div>
+        <p>현재 위치</p>
+      </div>
+      <div>
+        <p>진료과목</p>
+      </div>
+    </section>
+  );
 }
